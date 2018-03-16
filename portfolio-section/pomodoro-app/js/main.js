819e73e7e643379ea6cf7302f9913f8583f517d4
Anastasiya Trypuz <anastasiya.trypuz@gmail.com>
2018-03-16 19:40:05 +0100

    Improved Pomodoro Timer, updated CV file

--- portfolio-section/pomodoro-app/js/main.js
+++ portfolio-section/pomodoro-app/js/main.js
@@ -3,40 +3,42 @@
 $(document).ready(function() {
     
     var count = parseInt($('#session-num').html());
-    var breakTime = parseInt($('#break-num').html());
+    var timeBreak = parseInt($('#break-num').html());
 
-    $('#minus5Session').click(function() {
+    $('#minus-min-session').click(function() {
         if (count > 5) {
             count -= 5;
             $('#session-num').html(count);
         }
     });
     
-    $('#add5Session').click(function() {
+    $('#add-min-session').click(function() {
         count += 5;
         $('#session-num').html(count);
     }); 
     
-    $('#minus5Break').click(function () {
-        if (breakTime > 5) {
-            breakTime -= 5;
-            $('#break-num').html(breakTime);
+    $('#minus-min-break').click(function () {
+        if (timeBreak > 5) {
+            timeBreak -= 5;
+            $('#break-num').html(timeBreak);
         }
     });
 
-    $('#add5Break').click(function () {
-        breakTime += 5;
-        $('#break-num').html(breakTime);
+    $('#add-min-break').click(function () {
+        timeBreak += 5;
+        $('#break-num').html(timeBreak);
     });
     
     $('#start').click(function() {
         
         count *= 60;
         
-        var counter = setInterval(timer, 1000);
+        var sessionCounter = setInterval(sessionTimer, 1000);
         
-        function timer(){
-
+        function sessionTimer(){
+            
+            $('#start').hide();
+            
             $('#timer-title').html('Session Time:');
             count -= 1;
             
@@ -48,71 +50,44 @@ $(document).ready(function() {
             }
 
             if (count === 0) {
-                clearInterval(counter);
-                breakTime *= 60;
-                var startBreak = setInterval(breakTimer, 1000);
+                clearInterval(sessionCounter);
+                
+                timeBreak *= 60;
+                var breakCounter = setInterval(breakTimer, 1000);
                 
                 function breakTimer() {
                     
                     $('#timer-title').html('Break Time:');
-                    breakTime -= 1;
+                    timeBreak -= 1;
                     
-                    if (breakTime%60 >= 10) {
-                        $('.timer-count').html(Math.floor(breakTime/60) + ':' + breakTime%60);  
+                    if (timeBreak%60 >= 10) {
+                        $('.timer-count').html(Math.floor(timeBreak/60) + ':' + timeBreak%60);  
                     }
                     else {
-                        $('.timer-count').html(Math.floor(breakTime/60) + ':' + '0' + breakTime%60); 
+                        $('.timer-count').html(Math.floor(timeBreak/60) + ':' + '0' + timeBreak%60); 
                     }
                     
-                    if (breakTime === 0) {
-                        clearInterval(startBreak);
-//                        $('#break-num').hide();
+                    if (timeBreak === 0) {
+                        clearInterval(breakCounter);
+                        $('#start').show();
                     }
                 }
-                
-                $('#stop').click (function() {
-                    clearInterval(startBreak);
-                    $('#timer-title').html('Stoped');
-                });
-                
-                $('#reset').click(function() {
-                    clearInterval(startBreak);
-                    count = 25;
-                    breakTime = 25;
-                    $('#session-num').html(count);
-                    $('#break-num').html(breakTime);
-
-                    $('#timer-title').html('Timer:');
-                    $('.timer-count').html('00:00');
-                });
             }
         }
         
-        $('#stop').click (function() {
-            clearInterval(counter);
-            $('#timer-title').html('Stopped');
-        });
-        
         $('#reset').click(function() {
-            clearInterval(counter);
+            clearInterval(sessionCounter);
 
             count = 25;
-            breakTime = 25;
+            timeBreak = 25;
             $('#session-num').html(count);
-            $('#break-num').html(breakTime);
+            $('#break-num').html(timeBreak);
 
             $('#timer-title').html('Timer:');
             $('.timer-count').html('00:00');
+            
+            $('#start').show();
         });
-    });
-    
-    $('#reset').click(function() {
-
-        count = 25;
-        breakTime = 25;
-        $('#session-num').html(count);
-        $('#break-num').html(breakTime);
-
     });
 });
 
